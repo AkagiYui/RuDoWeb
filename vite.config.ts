452d52c1ecab8fd793c16592053f4d5359dc579f
AkagiYui <akagiyui@yeah.net>
2024-07-24 00:05:11 +0800

    chore: add vite server config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5174,
+  },
+  preview: {
+    host: "0.0.0.0",
+  },
   plugins: [react()],
 })
